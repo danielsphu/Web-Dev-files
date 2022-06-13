fbcf83d49e9786b20da4add50c7b148a972dd39a
Daniel Hu <65648964+danielsphu@users.noreply.github.com>
2022-06-12 23:51:41 -0400

    Changes

--- Score Keeper/app.js
+++ Score Keeper/app.js
@@ -4,12 +4,25 @@ const p1Display = document.querySelector("#p1Display")
 const p2Display = document.querySelector("#p2Display")
 
 let p1Score = 0;
+let p2Score = 0;
 let winningScore = 5;
+let isGameOver = false;
 
 p1Button.addEventListener('click', () =>{
-    if(p1Score !== winningScore){
+    if(!isGameOver){
         p1Score += 1;
+        if (p1Score === winningScore){
+            isGameOver=True
+        }
         p1Display.textContent = p1Score;
     }
-    
+})
+p2Button.addEventListener('click', () =>{
+    if(!isGameOver){
+        p2Score += 1;
+        if (p2Score === winningScore){
+            isGameOver=True
+        }
+        p1Display.textContent = p2Score;
+    }
 })
