faa0c6c1683f04227248bff8339fc1a6ce9988d3
Daniel Hu <65648964+danielsphu@users.noreply.github.com>
2022-06-17 16:49:49 -0400

    Trying out the integration of web apis with axiom and the DOM

---
+++ TV show search app/App.js
@@ -0,0 +1,19 @@
+const form = document.querySelector("#searchForm")
+form.addEventListener('submit', async function(e){
+    e.preventDefault();
+    const searchterm = form.elements.query.value;
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`, {params: {q: searchTerm}})
+    console.log(res.data[0].show.image.medium);
+    createimages(res.data);
+    form.elements.query.value = ""
+})
+
+const createimages = (shows) =>{
+    for (let result of shows){
+        if(result.show.image){
+            const img = document.createElement('IMG');
+            img.src = result.show.image.medium;
+            document.body.append(img)
+        } 
+    }
+}
