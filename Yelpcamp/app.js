6c41dbb83e02ceb4cffe26a40d612f4f98d22ab2
Daniel Hu <65648964+danielsphu@users.noreply.github.com>
2022-07-20 00:14:17 -0400

    Added some basic styling with Bootstrap boilerplate

--- Yelpcamp/app.js
+++ Yelpcamp/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
 const {transcode} = require("buffer");
+const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp");
@@ -15,6 +16,7 @@ db.once("open", ()=> {
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
