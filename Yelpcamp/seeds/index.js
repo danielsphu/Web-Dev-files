150d5fedcbe27b25a6dd5625e072e40a3a14ce0e
Daniel Hu <65648964+danielsphu@users.noreply.github.com>
2022-07-20 23:46:13 -0400

    Basic bootstrap styling added

--- Yelpcamp/seeds/index.js
+++ Yelpcamp/seeds/index.js
@@ -17,10 +17,14 @@ const sample = (array)=> array[Math.floor(Math.random()*array.length)]
 const seedDB = async () =>{
     await Campground.deleteMany({});
     for (let i = 0; i<50; i++){
+        const price = Math.floor(Math.random()*20)+10;
         const random1000 = Math.floor(Math.random()*1000);
         const camp = new Campground ({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: "https://placeimg.com/640/480/nature",
+            description:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat labore nam at et quos voluptate sapiente perferendis voluptatibus tempore repellendus rem accusantium incidunt dolorum ipsam numquam quo quia, obcaecati esse.",
+            price: price,
         })
         await camp.save();
     }
